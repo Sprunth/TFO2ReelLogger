97a47da8940b772d005b3343fa265de8c769c78d
Dylan Wang <dylan.wang@gmail.com>
2016-07-12 23:56:00 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,8 @@
+#  TFO2 Reel Logger
+
+A python script that stores the Reel Log from [Trophy Fishing Online 2](http://trophyfishingonline.com/) into an SQLite database.
+
+# To Run
+Save your Reel Log as reellog.html into a folder named html_data.
+Run db.py from a python console with BeautifulSoup4 installed (pip install beautifulsoup4 or use the environment.yml conda environment)
+A reellog.db file will be produced. Use any SQLite3 viewer to view, such as [DB Browser for SQLite](http://sqlitebrowser.org/)
---
+++ Scraper.py
@@ -0,0 +1,50 @@
+from bs4 import BeautifulSoup
+from pprint import pprint
+from functools import reduce
+
+
+def scrape():
+
+    soup = BeautifulSoup(open('html_data/reellog.html'), 'html.parser')
+
+    rows = soup.find_all('tr')
+
+    commands = list()
+
+    for row in rows[1:]:
+        cols = row.find_all('td')
+
+        lure_string = list(cols[0].descendants)[0]
+        lure = lure_string.find_all('img')[0].string
+
+        body_of_water = cols[1].string
+
+        location = cols[2].string
+
+        fish_string = cols[3]
+        fish_type = fish_string.font.string
+        fish_level = fish_string.img.string.split()[-1]
+
+        size_strings = list(map(lambda x: x.string, cols[4].find_all('font')))
+        weight_idx = -1
+        for idx in range(len(size_strings)):
+            if 'lb' in size_strings[idx]:
+                weight_idx = idx
+                break
+        weight = size_strings[weight_idx].split()[0]
+        fish_class = reduce(lambda x, y: "%s %s" % (x, y), size_strings[weight_idx+1:])
+        if 'L e g e n d a r y' in fish_class:
+            fish_class = 'Legendary'
+
+        # size not stored for now
+        # size = reduce(lambda x, y: "%s %s" % (x, y), size_strings[:-3])
+
+        command = "'%s', '%s', '%s', '%s', '%s', '%s', '%s'" % (lure, body_of_water, location, fish_type, fish_level,
+                                                                weight, fish_class)
+        commands.append(command)
+
+    return commands
+
+if __name__ == '__main__':
+    scrape_data = scrape()
+    pprint(scrape_data)
